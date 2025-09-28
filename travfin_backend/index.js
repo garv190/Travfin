93477f509cd194cc8a2c63ceacf54ab56eeae040
garv190 <garv.jauhari@gmail.com>
2025-09-28 19:06:43 +0530

    cors problem

--- travfin_backend/index.js
+++ travfin_backend/index.js
@@ -19,12 +19,37 @@ import './db.js';
 import mongoose from 'mongoose';
 const { sign, verify } = jwt;
 
-app.use(cors({
-  origin: ['https://travfin-carefree-travel.netlify.app'], 
+// app.use(cors({
+//   origin: ['https://travfin-carefree-travel.netlify.app'], 
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+
+
+const corsOptions = {
+  origin: [
+    'https://travfin-carefree-travel.netlify.app',
+    'http://localhost:3000', 
+    // 'http://localhost:5173'  
+  ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type', 
+    'Authorization', 
+    'Access-Control-Allow-Credentials',
+    'Access-Control-Allow-Origin'
+  ],
+  preflightContinue: false,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
+
 
 app.use(express.json());
 app.use(cookieParser());
